cec050ff7678695e1bd526782de3549399bc047f
dinnguyen1495 <minhdinhnguyen1495@gmail.com>
2019-06-18 21:32:56 +0200

    Update: make it shorter, same logic

--- DC36.py
+++ DC36.py
@@ -1,7 +1,7 @@
 # Daily Coding 36
 # Given the root to a binary search tree, find the second largest node in the tree.
 
-class BSNode:
+class BSTree:
     def __init__(self, v):
         self.value = v
         self.left = None
@@ -14,59 +14,45 @@ class BSNode:
     def add(self, value):
         if value < self.value:
             if self.left is None:
-                self.left = BSNode(value)  
+                self.left = BSTree(value)  
             else:
                 self.left.add(value)
         if value > self.value:
             if self.right is None:
-                self.right = BSNode(value)  
+                self.right = BSTree(value) 
             else: 
                 self.right.add(value)
 
-    def largest_node(self):
-        max_node = self
+    def largest_node_and_parent(self):
+        max_node, parent = self, None
         while max_node.right is not None:
+            parent = max_node
             max_node = max_node.right
-        return max_node
+        return max_node, parent
 
     def second_largest_node(self):
         if self.left is None and self.right is None:
             return None
         if self.right is None:
-            return self.left.largest_node()
-        second_max = self
-        max_right = self.right
-        while max_right.right is not None:
-            second_max = max_right
-            max_right = max_right.right
-        if max_right.left is not None:
-            temp = max_right.left.largest_node()
-            if temp.value > second_max.value:
-                second_max = temp  
-        return second_max.value
-
-class BSTree:
-    def __init__(self, node = None):
-        self.root = node
-
-    def __str__(self):
-        return self.root.__str__()
-
-    def add(self, value):
-        self.root.add(value)    
-
-    def second_largest_node(self):
-        return self.root.second_largest_node()
+            result, _ = self.left.largest_node_and_parent()
+            return result
+        max_node, result = self.right.largest_node_and_parent()
+        if max_node.left is not None:
+            temp, _ = max_node.left.largest_node_and_parent()
+            if temp.value > result.value:
+                result = temp
+        return result.value
 
 def main():    
-    tree = BSTree(BSNode(50)) 
+    tree = BSTree(50)
     tree.add(30)
     tree.add(20)
     tree.add(40)
     tree.add(70)
     tree.add(60)
     tree.add(80)
-    print("BSTree:", tree)
+    tree.add(75)
+    print("Test BSTree:", tree)
     print("Second largest node:", tree.second_largest_node())
 
 if __name__ == '__main__':
